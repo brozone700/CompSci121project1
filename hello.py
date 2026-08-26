f04837d800138da5ebf8aee3dfc5946c8964e95d
brozone700 <brozone700@gmail.com>
2026-08-26 18:18:55 +0000

    commit for the assignment

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello Professor!")
+
+cheeseChecker = input("DO NOT TYPE and enter Cheese ")
+if cheeseChecker == "Cheese":
+    while True:
+        print("Cheese")
+else:
+    print("Good Job :D")
